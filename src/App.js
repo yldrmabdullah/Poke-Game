a91d368af18964a388fb02e94aa08f002cf9b9da
yldrmabdullah <abdullahahmetyildirim@gmail.com>
2023-01-12 15:19:45 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Pokegame from "./PokeGame/PokeGame";
+import "./PokeCard/PokeCard.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Go Card Game</h1>
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
